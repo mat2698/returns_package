b2390f5627aac9d76ad5b9834e3e619c194d1852
martinmat98 <martin.mat@husky.neu.edu>
2020-02-21 13:57:19 -0500

    name change

---
+++ finance_data_pull/__init__.py
@@ -0,0 +1,2 @@
+name = "finance_data_pull"
+
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="python_world",
+    name="finance_data_pull",
     version="0.0.1",
     author="Cedd Burge",
     author_email="ceddlyburge@gmail.com",
     description="A function that returns 'world'",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ceddlyburge/python_world",
+    url="https://github.com/mat2698/returns_package",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
